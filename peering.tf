a7e720976084507199ba29e551743287f525acda
hemasuraj879 <your_email@example.com>
2024-12-11 16:18:26 +0530

    files updating

--- peering.tf
+++ peering.tf
@@ -28,7 +28,7 @@ resource "aws_route" "private_peer" {
 
 resource "aws_route" "default" {
     count = var.peering_reqq ? 1 : 0
-  route_table_id            = aws_route_table.default.id
+  route_table_id            = data.aws_route_table.main.id
   destination_cidr_block    = var.vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peer[count.index].id
 }
